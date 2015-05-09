12a88dbdddef67565486a031c24b227a8a365e7e
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-05-09 16:27:26 -0700

    starts building out new Objects exercises

---
+++ Week1/day1Objects.js
@@ -0,0 +1,26 @@
+//Today we're going to get in a bunch of practice with objects. 
+//As you start your JS journey, there's a ton of value in just building out the muscle memory for the fundamentals. 
+//To get these fundamentals as deeply embedded as possible, type them out new each time. Avoid copying code, as you're just ignoring a chance to get in more useful practice. 
+
+// 1. To start, let's create a variable, mysticalAnimal, and set it equal to an object literal. 
+
+// Dot Notation:
+// Recall that dot notation takes whatever is to the right of the dot, turns it into a string, and looks up that string as a property name in the object to the left of the dot. 
+// Let's use dot notation to add some properties to mysticalAnimal.
+// 2. Add a type property, and set it equal to 'dragon'.
+// 3. Add a name property, and set it equal to 'Larry'.
+// 4. Add a likes property, and set it equal to an empty array.
+// 5. Add a property called bestFriend, and set it equal to your name. 
+// 6. Add in a superPower property. Set it equal to 'gives amazing hugs'.
+
+// Bracket notation:
+// Recall that we can do two different things with bracket notation:
+  // A. Evaluate a variable or expression, and look up what that variable evalutates to as a property name in the object.
+  // B. Pass in a string (as noted by the quotes surrounding it) and look up that string as a property name in the object. 
+// Remember that bracket notation is the ONLY way to look up variables or expressions when using them with objects. 
+
+// 7. Create a variable called thisIsAVariable. Set it equal to the string 'superPower'.
+// Now let's use this to play with bracket vs. dot notation. 
+// 8. 
+
+// grandCouncil - a collection
