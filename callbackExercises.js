a95ce39eb50bb883cbe8c2bed367f3a453528f17
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-05-09 16:26:12 -0700

    sparse ideas for callbacks

---
+++ callbackExercises.js
@@ -0,0 +1,5 @@
+// Build this back into the index.html file. 
+// build out a click handler? 
+// let's get creative here. how else can we pass in callback functions? 
+// setInterval- give them an explanation of what this is and what it does. 
+// 
