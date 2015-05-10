f28f7aa4a98eff1bb0a7e9c3ce3fe65f96425027
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-05-09 21:58:42 -0700

    outline of topics to cover

---
+++ Week1/day2Arrays.js
@@ -0,0 +1,23 @@
+// Arrays!
+
+// Arrays are great places to store lists of similar information.
+// Where objects have property names to denote the different slots you can store data in, arrays have numbered indices. 
+
+// 0-indexed
+// array.length
+// push
+// pop
+// unshift
+// shift
+// numerically indexed
+// accessing using variables
+// create an array of mysticalAnimals called grandCouncil
+// you access data from an array by just asking for the value stored at a certain numbered position. 
+
+// Arrays are 0-indexed. That is, the first thing that you put in there will go in at index 0. So if you have 3 things in your array, they will be at arr[0], arr[1], and arr[2]. 
+
+// 1. Create an empty array called treasureChest. We're going to fill it with all the things that our dragon from yesterday has hoarded over the years. 
+// 2. Let's make the value at position 0 'fur jackets'. 
+// Suddenly, our dragon is either A) attacked by PETA for having animal fur jackets, or B) decides to move somewhere warm. Either way, she's no longer in need of fur jackets. 
+// 3. Overwrite the value stored at position 0 to be 'ethically sourced sandals'. 
+// 4. 
